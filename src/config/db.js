6dcc9e623603294c615fe6486310ab51b8dea2d0
joaquinzup <joacozupel@gmail.com>
2026-05-14 20:23:38 -0300

    Subiendo funciones basicas

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import { env } from './env.js'
+const connectDB = async () => {
+    try {
+        console.log('💻 Conectando MongoDB...')
+        await mongoose.connect(env.MONGO_URL)
+        console.log('MongoDb conectado correctamente...')
+    } catch(error){
+        console.log('Error conectando MongoDB')
+    }
+}
