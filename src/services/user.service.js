0970b98e9a894f5c0d0aa021bc3c03eb55a90464
joaquinzup <joacozupel@gmail.com>
2026-05-21 20:16:44 -0300

    Se agrego las capas dto y models

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,13 @@
+import bcrypt from "bcryptjs"
+
+import User from '../models/user.model.js'
+
+import Audit from '../models/audit.model.js'
 const getUsersService = async () => {
     try {
         console.log('📦 SERVICE ➡️ getUserService')
-        return []
+        const users = await User.find().select('-password')
+        return
     } catch (error) {
         throw error
     }
@@ -11,6 +17,39 @@ const createUserService = async (data) => {
     try {
         console.log('📦 SERVICE ➡️ createUserService')
         console.log(data)
+        const existUser = await User.findOne({
+            email: data.email
+        })
+        if (existUser) {
+            throw new Error('Usuario Ya Existe')
+        }
+        const hashedPassword = await bcrypt.hash(
+            data.password,
+            10
+        )
+        const user = new User({
+            nombre: data.nombre,
+            apellido: data.apellido,
+            email: data.email,
+            password: hashedPassword,
+            edad: data.edad,
+            sexo: data.sexo,
+            telefono: data.telefono,
+            direccion: data.direccion
+        })
+        await user.save()
+
+        return {
+            id: user._id,
+           nombre: user.nombre,
+            apellido: user.apellido,
+            email: user.email,
+            edad: user.edad,
+            sexo: user.sexo,
+            telefono: user.telefono,
+            direccion: user.direccion 
+        }
+
         return data
     } catch (error) {
         throw error
